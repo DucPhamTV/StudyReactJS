43a8c8450844bd7095c2043ab791c0dc445fc325
DucPham <41200850@hcmut.edu.vn>
2020-10-09 23:24:32 +0700

    Game completed!

--- src/index.js
+++ src/index.js
@@ -3,6 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+
 function Square(props) {
   return (
     <button
@@ -27,6 +48,9 @@ class Board extends React.Component {
   handleClick(i) {
     const isXTune = !this.state.isXTune;
     const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+	}
     squares[i] = isXTune ? 'X' : 'O';
     this.setState({
       squares: squares,
@@ -41,8 +65,13 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: ' + (this.state.isXTune ? 'O' : 'X');
-
+	const result = calculateWinner(this.state.squares);
+    let status;
+    if (result) {
+      status = 'Winner is ' + result;
+    } else {
+      status = 'Next player: ' + (this.state.isXTune ? 'O' : 'X');
+    }
     return (
       <div>
         <div className="status">{status}</div>
